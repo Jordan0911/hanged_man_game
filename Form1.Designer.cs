6aa966c2009086670eb597e846108548b43b5fef
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-05-13 13:35:51 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace hanged_man_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Cyan;
+            textBox1.ForeColor = Color.Firebrick;
+            textBox1.Location = new Point(212, 289);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(289, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(332, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(516, 58);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(181, 225);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+namespace hanged_man_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        static string[] Word_chooser(int difficulty)
+        {
+            string[] raw = File.ReadAllLines("words.txt");
+            Random roll = new Random();
+            string[] arguments = { "sports", "war", "animals" };
+            string[] result = new string[2];
+            string chosen_word = "";
+            int chosen_argument = roll.Next(0, 3), a = 0;
+            string raw_word = "";
+            
+
+            if (difficulty == 1)
+            {
+                a = roll.Next(0, 6);
+            }
+            if (difficulty == 2)
+            {
+                a = roll.Next(6, 11);
+            }
+            if (difficulty == 3)
+            {
+                a = roll.Next(10, 16);
+            }
+            if (chosen_argument == 0)
+            {
+                chosen_word ="";
+            }
+            else if (chosen_argument == 1)
+            {
+                chosen_word = war[a];
+            }
+            else
+            {
+                chosen_word = animals[a];
+            }
+
+            result[0] = chosen_word;
+            result[1] = arguments[chosen_argument];
+            return result;
+        }
+
+    }
+}

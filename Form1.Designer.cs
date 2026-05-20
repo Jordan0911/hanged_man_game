4963f5541efa3c30e4aba18b8ebaa57aa45daa7f
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-05-20 13:35:59 +0200

    w

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,135 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.letter_chosen = new System.Windows.Forms.TextBox();
-            this.word = new System.Windows.Forms.Label();
-            this.argument = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.confirm = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.n_lives = new System.Windows.Forms.Label();
-            this.final_word = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            letter_chosen = new TextBox();
+            word = new Label();
+            argument = new Label();
+            label5 = new Label();
+            confirm = new Button();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            n_lives = new Label();
+            final_word = new Label();
+            SuspendLayout();
             // 
             // letter_chosen
             // 
-            this.letter_chosen.Location = new System.Drawing.Point(231, 335);
-            this.letter_chosen.Name = "letter_chosen";
-            this.letter_chosen.Size = new System.Drawing.Size(214, 20);
-            this.letter_chosen.TabIndex = 0;
-            this.letter_chosen.TextChanged += new System.EventHandler(this.letter_chosen_TextChanged);
+            letter_chosen.Location = new Point(231, 335);
+            letter_chosen.Name = "letter_chosen";
+            letter_chosen.Size = new Size(214, 23);
+            letter_chosen.TabIndex = 0;
+            letter_chosen.TextChanged += letter_chosen_TextChanged;
             // 
             // word
             // 
-            this.word.AutoSize = true;
-            this.word.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.word.Font = new System.Drawing.Font("Playbill", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.word.Location = new System.Drawing.Point(202, 191);
-            this.word.Name = "word";
-            this.word.Size = new System.Drawing.Size(255, 65);
-            this.word.TabIndex = 1;
-            this.word.Text = "censured_word";
-            this.word.Click += new System.EventHandler(this.censured_word_Click);
+            word.AutoSize = true;
+            word.BackColor = Color.FromArgb(255, 128, 128);
+            word.Font = new Font("Playbill", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            word.Location = new Point(254, 186);
+            word.Name = "word";
+            word.Size = new Size(157, 65);
+            word.TabIndex = 1;
+            word.Text = "greetings";
+            word.TextAlign = ContentAlignment.MiddleCenter;
+            word.Click += censured_word_Click;
             // 
             // argument
             // 
-            this.argument.AutoSize = true;
-            this.argument.Font = new System.Drawing.Font("OCR A Extended", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.argument.Location = new System.Drawing.Point(276, 281);
-            this.argument.Name = "argument";
-            this.argument.Size = new System.Drawing.Size(115, 29);
-            this.argument.TabIndex = 2;
-            this.argument.Text = "label4";
+            argument.AutoSize = true;
+            argument.Font = new Font("OCR A Extended", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            argument.Location = new Point(256, 280);
+            argument.Name = "argument";
+            argument.Size = new Size(149, 29);
+            argument.TabIndex = 2;
+            argument.Text = "argument";
+            argument.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(311, 70);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 13);
-            this.label5.TabIndex = 3;
-            this.label5.Click += new System.EventHandler(this.label5_Click);
+            label5.AutoSize = true;
+            label5.Location = new Point(311, 70);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 15);
+            label5.TabIndex = 3;
+            label5.Click += label5_Click;
             // 
             // confirm
             // 
-            this.confirm.Location = new System.Drawing.Point(256, 380);
-            this.confirm.Name = "confirm";
-            this.confirm.Size = new System.Drawing.Size(155, 59);
-            this.confirm.TabIndex = 4;
-            this.confirm.Text = "button5";
-            this.confirm.UseVisualStyleBackColor = true;
-            this.confirm.Click += new System.EventHandler(this.conferm_Click);
+            confirm.Location = new Point(256, 380);
+            confirm.Name = "confirm";
+            confirm.Size = new Size(155, 59);
+            confirm.TabIndex = 4;
+            confirm.Text = "button5";
+            confirm.UseVisualStyleBackColor = true;
+            confirm.Click += conferm_Click;
             // 
             // checkBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(85, 100);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(49, 17);
-            this.checkBox1.TabIndex = 5;
-            this.checkBox1.Text = "Easy";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(85, 100);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(49, 19);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "Easy";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
             // 
             // checkBox2
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(85, 124);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(63, 17);
-            this.checkBox2.TabIndex = 6;
-            this.checkBox2.Text = "Medium";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(85, 124);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(71, 19);
+            checkBox2.TabIndex = 6;
+            checkBox2.Text = "Medium";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
             // 
             // checkBox3
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(85, 148);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(61, 17);
-            this.checkBox3.TabIndex = 7;
-            this.checkBox3.Text = "Difficult";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(85, 148);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(68, 19);
+            checkBox3.TabIndex = 7;
+            checkBox3.Text = "Difficult";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
             // 
             // n_lives
             // 
-            this.n_lives.AutoSize = true;
-            this.n_lives.Location = new System.Drawing.Point(147, 342);
-            this.n_lives.Name = "n_lives";
-            this.n_lives.Size = new System.Drawing.Size(78, 13);
-            this.n_lives.TabIndex = 8;
-            this.n_lives.Text = "number of lives";
+            n_lives.AutoSize = true;
+            n_lives.Location = new Point(147, 342);
+            n_lives.Name = "n_lives";
+            n_lives.Size = new Size(89, 15);
+            n_lives.TabIndex = 8;
+            n_lives.Text = "number of lives";
             // 
             // final_word
             // 
-            this.final_word.AutoSize = true;
-            this.final_word.Font = new System.Drawing.Font("Old English Text MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.final_word.Location = new System.Drawing.Point(273, 112);
-            this.final_word.Name = "final_word";
-            this.final_word.Size = new System.Drawing.Size(113, 44);
-            this.final_word.TabIndex = 9;
-            this.final_word.Text = "label6";
+            final_word.AutoSize = true;
+            final_word.Font = new Font("Old English Text MT", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            final_word.Location = new Point(273, 112);
+            final_word.Name = "final_word";
+            final_word.Size = new Size(113, 44);
+            final_word.TabIndex = 9;
+            final_word.Text = "label6";
+            final_word.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(710, 623);
-            this.Controls.Add(this.final_word);
-            this.Controls.Add(this.n_lives);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.confirm);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.argument);
-            this.Controls.Add(this.word);
-            this.Controls.Add(this.letter_chosen);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            ClientSize = new Size(710, 623);
+            Controls.Add(final_word);
+            Controls.Add(n_lives);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(confirm);
+            Controls.Add(label5);
+            Controls.Add(argument);
+            Controls.Add(word);
+            Controls.Add(letter_chosen);
+            Name = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,15 @@
 namespace hanged_man_game
 {
-   public partial class Form1 : Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
             final_word.Hide();
         }
-        
 
-        
+
+
         static string[] Word_chooser(int difficulty)
         {
             string[] raw = File.ReadAllLines("words.txt");
@@ -54,7 +54,7 @@ namespace hanged_man_game
 
         private void letter_chosen_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void label5_Click(object sender, EventArgs e)
@@ -64,49 +64,47 @@ namespace hanged_man_game
         private void conferm_Click(object sender, EventArgs e)
         {
             int lives = Convert.ToInt32(n_lives.Text);
-            bool letter_found=false;
+            bool letter_found = false;
             char letter = Convert.ToChar(letter_chosen.Text);
-            string placeholder_2 = "";
+            string result = "";
             string decided_word = final_word.Text;
-            char[] placeholder_censured=word.Text.ToCharArray();
-            if (word.Text == "censured_word")
+            char[] Censured_word = new char[final_word.Text.Length];
+            if (word.Text == "greetings")
             {
-                for (int i = 0; i < placeholder_censured.Length; i++)
+                for (int i = 0; i < decided_word.Length; i++)
                 {
-                    placeholder_censured[i] = '_';
+                    Censured_word[i] = '_';
                 }
             }
-            
 
-            for (int i = 0; i < decided_word.Length; i++) 
+
+            for (int i = 0; i < decided_word.Length; i++)
             {
-                if (letter == decided_word[i]) 
+                if (letter == decided_word[i])
                 {
-                    placeholder_censured[i] = letter;                
-                    letter_found=true;
+                    Censured_word[i] = letter;
+                    letter_found = true;
                 }
             }
-            if (letter_found == false) 
+            if (letter_found == false)
             {
                 lives--;
-                
-                n_lives.Text=Convert.ToString(lives);
+                if (lives == 0)
+                {
+                    word.Text ="YOU LOST!!!";
+
+                }
+                n_lives.Text = Convert.ToString(lives);
             }
-            for(int i=0;i<decided_word.Length; i++)
+            for (int i = 0; i < decided_word.Length; i++)
             {
-                placeholder_2 += placeholder_censured[i];
+               result += Censured_word[i];
             }
-            word.Text = placeholder_2;
+            word.Text = result;
         }
-
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBox1.Checked) 
+            if (checkBox1.Checked)
             {
                 n_lives.Text = "12";
                 checkBox1.Hide();
@@ -128,7 +126,7 @@ namespace hanged_man_game
                 checkBox1.Hide();
                 checkBox2.Hide();
                 checkBox3.Hide();
-                string placeholder=n_lives.Text;
+                string placeholder = n_lives.Text;
                 int difficulty = Convert.ToInt32(placeholder);
                 string[] stuff = Word_chooser(difficulty);
                 final_word.Text = stuff[0];
@@ -153,6 +151,11 @@ namespace hanged_man_game
         }
 
         private void censured_word_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
